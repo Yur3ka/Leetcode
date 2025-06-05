29c508066a82b56bad1ccd6495c9f9004ff61de3
Yur3ka <105861686+Yur3ka@users.noreply.github.com>
2025-06-05 12:03:36 +0700

    Time: 22 ms (29.48%) | Memory: 26.8 MB (6.81%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,9 +1,9 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = [0]*(n+1)
-        queue = [(0,0)]
+        queue = deque([(0,0)])
         while queue:
-            i, bit = queue.pop(0)
+            i, bit = queue.popleft()
             if i > n:
                 break
             ans[i] = bit
