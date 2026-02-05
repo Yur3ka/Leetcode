c13527011e02baceb38d05a8a07ec83d1dfd43a3
Yur3ka <105861686+Yur3ka@users.noreply.github.com>
2026-02-05 09:44:15 +0700

    Time: 70 ms (7.17%) | Memory: 19.4 MB (34.53%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0]*n
+        for i in range(n):
+            if nums[i] == 0:
+                res[i] = 0
+            else:
+                idx = (i+nums[i]) % n
+                res[i] = nums[idx]
+        return res
